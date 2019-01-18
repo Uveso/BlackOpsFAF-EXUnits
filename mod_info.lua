267558b4819d85f63dffc681a1873058be5a1178
Uveso <Archivar@Zwergenteam.de>
2019-01-18 01:14:55 +0100

    Initial commit

---
+++ mod_info.lua
@@ -0,0 +1,21 @@
+name = "BlackOps FAF: EXUnits"
+uid = "9e8ee941-c406-4751-b367-e10000017000"
+version = 17
+copyright = "2009 Exavier Macbeth"
+description = "The ultimate unit pack, re-engineered for FAF 3680 or later"
+author = "Exavier Macbeth, lt_hawkeye, orangeknight - Revamped 2016 by IceDreamer and Uveso"
+icon = "/mods/BlackOpsFAF-EXUnits/icon.png"
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
+requires = {}
+requiresNames = {}
+conflicts = {
+    "9e8ee941-c306-4751-b367-e10000000104",
+    "9e8ee941-c306-4751-b367-e10000014000",
+    "9e8ee941-c406-4751-b367-e10000015000",
+    "9e8ee941-c406-4751-b367-e10000016000",
+}
+before = {}
+after = {}
