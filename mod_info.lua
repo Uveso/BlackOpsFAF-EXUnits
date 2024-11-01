09f549c7720b2cc1e8bbd749e59d779246ee70ce
Uveso <Archivar@Zwergenteam.de>
2024-11-02 00:44:22 +0100

    Update v22

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "BlackOps FAF: EXUnits"
-uid = "9e8ee941-c406-4751-b367-e10000021000"
-version = 21
+uid = "9e8ee941-c406-4751-b367-e10000022000"
+version = 22
 copyright = "2009 Exavier Macbeth"
 description = "The ultimate unit pack, re-engineered for FAF 3680 or later"
 author = "Exavier Macbeth, lt_hawkeye, orangeknight"
